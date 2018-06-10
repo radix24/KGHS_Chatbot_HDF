035c3b8034b421c87dd7c89f43c1b41a758b637e
HyunSeoCho <36672748+HyunsSeoCho@users.noreply.github.com>
2018-06-11 02:11:33 +0900

    include

--- KGHSchatbot/message.php
+++ KGHSchatbot/message.php
@@ -5,6 +5,7 @@
     include("./functions/menu3.php");
     include("./functions/weather1.php");
     include("./functions/calander.php");
+    include("./functions/timetable.php");
     if ( strpos($content, "개발자") !== false ) {
       echo '{
             "message" :
